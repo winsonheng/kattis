cdd05fa4ad153843b854d25dbaff706207f645b3
winson heng <winsonh5147@gmail.com>
2022-12-05 22:39:12 +0800

    Completed Apaxians!

---
+++ src/Apaxiaaaaaaaaaaaans!/apaxiaaans.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.OutputStream;
+import java.util.StringTokenizer;
+
+class Kattio extends PrintWriter {
+    public Kattio(InputStream i) {
+        super(new BufferedOutputStream(System.out));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+    public Kattio(InputStream i, OutputStream o) {
+        super(new BufferedOutputStream(o));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public boolean hasMoreTokens() {
+        return peekToken() != null;
+    }
+
+    public int getInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    public double getDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+    public long getLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    public String getWord() {
+        return nextToken();
+    }
+
+
+
+    private BufferedReader r;
+    private String line;
+    private StringTokenizer st;
+    private String token;
+
+    private String peekToken() {
+        if (token == null)
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    line = r.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                }
+                token = st.nextToken();
+            } catch (IOException e) { }
+        return token;
+    }
+
+    private String nextToken() {
+        String ans = peekToken();
+        token = null;
+        return ans;
+    }
+}
+
+
+public class apaxiaaans {
+    public static void main(String[] args) {
+        Kattio k = new Kattio(System.in);
+        char recent = '.';
+        StringBuilder res = new StringBuilder();
+        String input = k.getWord();
+
+        for (int i = 0; i < input.length(); ++i) {
+            char c = input.charAt(i);
+            if (recent != c) {
+                recent = c;
+                res.append(c);
+            }
+        }
+
+        k.println(res.toString());
+        k.close();
+    }
+}
